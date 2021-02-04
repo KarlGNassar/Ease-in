12fd73c550eca03d2610936aa9a5882a3a711080
KARL NASSAR <kgnassar01@gmail.com>
2021-02-04 03:22:53 +0200

    fixed offset

--- src/components/VideoHero/index.js
+++ src/components/VideoHero/index.js
@@ -25,7 +25,7 @@ const HeroVideo = () => {
                     style={{maxWidth: '160px'}}
                     smooth={true}
                     duration={1000}
-                    offset={-55}
+                    offset={-60}
                 >
                     Get Started
                     <Arrow />
